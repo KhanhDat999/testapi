2c27f3643547a95bd8c4f10956af66986ca43e85
admin <admin@MacBook-Po-ro-cua-at-Khet-Let.local>
2022-12-08 18:19:16 +0700

    abc

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql') ;
 const port = 3000
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'nemshop',
+  user: 'root',
+  password: 'password',
+});
+
+connection.connect()
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  connection.query(`SELECT * FROM Banner`, (err, data) => {
+    if (err) throw err ;
+    res.json(data)
+   
+  });
+
+}),
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
